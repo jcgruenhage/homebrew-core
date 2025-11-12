c154fa93e4c9420483ee9bfb15e5df7a8dfc032f
Bevan Kay <email@bevankay.me>
2025-11-12 13:29:04 +1100

    dotnet 10.0.100

--- Formula/d/dotnet.rb
+++ Formula/d/dotnet.rb
@@ -7,14 +7,14 @@ class Dotnet < Formula
 
   stable do
     # Source-build tag announced at https://github.com/dotnet/source-build/discussions
-    version "9.0.8"
-    url "https://github.com/dotnet/dotnet/archive/refs/tags/v9.0.109.tar.gz"
-    sha256 "42fdfe3733884a3f6ceb3b428ff346ccb92f95010c447e27f3b164f70145730c"
+    version "10.0.0"
+    url "https://github.com/dotnet/dotnet/archive/refs/tags/v10.0.100.tar.gz"
+    sha256 "e190b1f43a2230a7aa31a1075bf00dcd7f4f81d8ec250c1b858f2261cc27be53"
 
     resource "release.json" do
-      version "9.0.8"
-      url "https://github.com/dotnet/dotnet/releases/download/v9.0.109/release.json"
-      sha256 "328388103bf81072b638984d05b9dff5b6520a366d6448abbc7b985539deb8f4"
+      version "10.0.0"
+      url "https://github.com/dotnet/dotnet/releases/download/v10.0.100/release.json"
+      sha256 "6a00650346267ebf17dc69465c69cbd2677ece264300c618c178f357f8de0c12"
 
       livecheck do
         formula :parent
